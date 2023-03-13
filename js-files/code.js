47f0577c56bac8845fbf4d3689b35b3863983d5a
DESKTOP-7AB9G8D\Geert <g.j.haans@students.uu.nl>
2023-03-13 11:26:12 +0100

    I love when sergey says 'on the fly'

--- js-files/code.js
+++ js-files/code.js
@@ -1,38 +1,3 @@
-const hamburger = document.querySelector(".nav-content__hamburger");
-const navMenu = document.querySelector(".nav-content__unordered-list");
-const html = document.querySelector("html");
-const body = document.querySelector("body");
-
-hamburger.addEventListener("click", () => {
-	hamburger.classList.toggle("active");
-	navMenu.classList.toggle("active");
-	html.classList.toggle("active");
-	body.classList.toggle("active");
-});
-
-document.querySelectorAll(".nav-link").forEach((n) =>
-	n.addEventListener("click", () => {
-		hamburger.classList.remove("active");
-		navMenu.classList.remove("active");
-		html.classList.remove("active");
-		body.classList.remove("active");
-	})
-);
-
-window.addEventListener("resize", function (event) {
-	var width =
-		window.innerWidth ||
-		document.documentElement.clientWidth ||
-		document.body.clientWidth;
-	if (width >= 1023) {
-		hamburger.classList.remove("active");
-		navMenu.classList.remove("active");
-		html.classList.remove("active");
-		body.classList.remove("active");
-	}
-});
-
-
 // var cursor = document.querySelector('.cursor');
 // var cursorinner = document.querySelector('.cursor2');
 // var a = document.querySelectorAll('a');
@@ -195,12 +160,6 @@ for (let i = 0; i < clickablePreview.length; i++) {
 		currentAdditional = null;
 	});
 }
-
-
-
-
-
-
 
 
 class Movie {
--- js-files/navInitialiser.js
+++ js-files/navInitialiser.js
@@ -1,4 +1,4 @@
-// CREATING NAV MOMENT
+// CREATING NAV
 const nav = document.getElementsByTagName("nav")[0];
 nav.classList.add("nav-content");
 const hamburgerDiv = document.createElement("div");
@@ -30,6 +30,7 @@ for(let i = 0; i < 5; i++){
 	liItem.appendChild(liA);
 	navUl.appendChild(liItem);
 }
+
 document.querySelectorAll(".nav-content__item--ref")[0].href = "./plot.html";
 document.querySelectorAll(".nav-content__item--ref")[0].textContent = "PLOT";
 document.querySelectorAll(".nav-content__item--ref")[1].href = "./cast.html";
@@ -40,3 +41,37 @@ document.querySelectorAll(".nav-content__item--ref")[3].href = "./reviews.html";
 document.querySelectorAll(".nav-content__item--ref")[3].textContent = "REVIEWS";
 document.querySelectorAll(".nav-content__item--ref")[4].href = "./info.html";
 document.querySelectorAll(".nav-content__item--ref")[4].textContent = "INFO";
+
+const hamburger = document.querySelector(".nav-content__hamburger");
+const navMenu = document.querySelector(".nav-content__unordered-list");
+const html = document.querySelector("html");
+const body = document.querySelector("body");
+
+hamburger.addEventListener("click", () => {
+	hamburger.classList.toggle("active");
+	navMenu.classList.toggle("active");
+	html.classList.toggle("active");
+	body.classList.toggle("active");
+});
+
+document.querySelectorAll(".nav-link").forEach((n) =>
+	n.addEventListener("click", () => {
+		hamburger.classList.remove("active");
+		navMenu.classList.remove("active");
+		html.classList.remove("active");
+		body.classList.remove("active");
+	})
+);
+
+window.addEventListener("resize", function (event) {
+	var width =
+		window.innerWidth ||
+		document.documentElement.clientWidth ||
+		document.body.clientWidth;
+	if (width >= 1023) {
+		hamburger.classList.remove("active");
+		navMenu.classList.remove("active");
+		html.classList.remove("active");
+		body.classList.remove("active");
+	}
+});
